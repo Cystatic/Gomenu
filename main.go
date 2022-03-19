b6d77d74c77facf2e1ad55a1df5f38226456a1a1
caosy <csy015676@163.com>
2022-03-19 16:17:13 +0800

    数组元素和

--- main.go
+++ main.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 )
 
+//求数组[1, 3, 5, 7, 8]所有元素的和
+func Sum() {
+	a := [5]int{1, 3, 5, 7, 8}
+	sum := 0
+	for _, i := range a {
+		sum += i
+	}
+	fmt.Println(sum)
+}
+
 func main() {
 	who := "world!"
 	fmt.Println("hello", who)
+	Sum()
 }
