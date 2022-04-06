4933b9b107b7e253773374a60c2d82df945a07d2
caosy <csy015676@163.com>
2022-04-06 22:53:24 +0800

    LinkTable库

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var showmenu = menu()
-	showmenu()
+	// var showmenu = menu()
+	// showmenu()
+
+	linktable := initLinkTable()
+	// fmt.Println(linktable.headNode)
+
+	// insertHeadNode(1, linktable)
+	// insertHeadNode(2, linktable)
+	// insertHeadNode(3, linktable)
+	// showLinkTableData(linktable)
+	// fmt.Println()
+
+	insertTailNode(4, linktable)
+	insertTailNode(5, linktable)
+	insertTailNode(6, linktable)
+	// showLinkTableData(linktable)de
+	// fmt.Println()
+	// append(3, 7, linktable)
+	// fmt.Println(findNodeByIndex(1, linktable))
+	// fmt.Println(findNodeByValue(6, linktable))
+
+	deleteNode(2, linktable)
+	showLinkTableData(linktable)
+
 }
 func menu() func() {
 	var message string = ""
